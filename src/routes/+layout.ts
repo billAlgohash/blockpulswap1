e52b2d38ad394f3e7cc1b62014a9294c00413f5c
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-28 15:19:48 +0800

    1 file modified

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,12 +1,13 @@
 // src/routes/+layout.ts
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { invalidate } from '$app/navigation'
+const  PUBLIC_SUPABASE_URL = 'https://amvtkeyfaduowfkbviyl.supabase.co'
+const  PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtdnRrZXlmYWR1b3dma2J2aXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2MDEzMTUsImV4cCI6MjAwMzE3NzMxNX0.n6XWDgimQY161SzrgcC9nSOzTAhMYHnstACz8nGtOVE'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
-import type { Database } from '../DatabaseDefinitions'
 
 export const load = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
 
-  const supabase = createSupabaseLoadClient<Database>({
+  const supabase = createSupabaseLoadClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
     event: { fetch },
