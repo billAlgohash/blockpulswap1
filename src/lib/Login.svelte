89b9f38fd71d785bbfc79046eac01a41d54ca4ef
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-28 22:39:39 +0800

    1 file modified

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,17 +1,20 @@
-<script>
+<script lang="ts">
   import { currentUser, pb } from './pocketbase';
-  let username = "";
-  let password = "";
 
-  async function login(){
-    await pb.collection('trader').authWithPassword(username,password);
+  let username: string;
+  let password: string;
+
+  async function login() {
+    await pb.collection('trader').authWithPassword(username, password);
   }
-  async function signUp(){
-    try{
+
+  async function signUp() {
+    try {
       const data = {
         username,
         password,
         passwordConfirm: password,
+        name: 'hi mom!',
       };
       const createdUser = await pb.collection('trader').create(data);
       await login();
@@ -19,21 +22,32 @@
       console.error(err)
     }
   }
-  function signOut(){
+
+  function signOut() {
     pb.authStore.clear();
   }
 
 </script>
 
 {#if $currentUser}
-  <p>Signed in as {$currentUser.username}</p>
-  <button on:click={signOut}>Sign Out</button>
+  <p>
+    Signed in as {$currentUser.username} 
+    <button on:click={signOut}>Sign Out</button>
+  </p>
 {:else}
   <form on:submit|preventDefault>
-  <input placeholder="username" type="text" bind:value={username}/>
-  <input placeholder="password" type="password" bind:value={password}/>
-  <button on:click={signUp}>Sign Up</button>
-  <button on:click={login}>Login</button>
+    <input
+      placeholder="Username"
+      type="text"
+      bind:value={username}
+    />
 
+    <input 
+      placeholder="Password" 
+      type="password" 
+      bind:value={password} 
+    />
+    <button on:click={signUp}>Sign Up</button>
+    <button on:click={login}>Login</button>
   </form>
 {/if}
