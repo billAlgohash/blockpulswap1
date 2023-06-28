02ff23fb695426845faa42e3ab15af9d87104be4
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-28 21:00:10 +0800

    2 files modified, 2 added

---
+++ src/lib/Login.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { currentUser, pb } from './pocketbase';
+  let username : string;
+  let password : string;
+  
+</script>
+
+{#if $currentUser}
+  <p>Signed in as {$currentUser.username}</p>
+{/if}
