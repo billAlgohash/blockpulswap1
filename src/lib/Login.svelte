f5dfd17b358f164189131bcb9993d5592c22d683
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-28 22:28:25 +0800

    1 file modified

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -2,9 +2,38 @@
   import { currentUser, pb } from './pocketbase';
   let username : string;
   let password : string;
-  
+
+  async function login(){
+    await pb.collection('trader').authWithPassword(username,password);
+  }
+  async function signUp(){
+    try{
+      const data = {
+        username,
+        password,
+        passwordConfirm: password,
+      };
+      const createdUser = await pb.collection('trader').create(data);
+      await login();
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  function signOut(){
+    pb.authStore.clear();
+  }
+
 </script>
 
 {#if $currentUser}
   <p>Signed in as {$currentUser.username}</p>
+  <button on:click={signOut}>Sign Out</button>
+{:else}
+  <form>
+  <input placeholder="username" type="text" bind:value={username}/>
+  <input placeholder="password" type="password" bind:value={password}/>
+  <button on:click={signUp}>Sign Up</button>
+  <button on:click={login}>Login</button>
+
+  </form>
 {/if}
