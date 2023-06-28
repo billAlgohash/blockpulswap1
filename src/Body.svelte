513905cac1379aa1001fc62f8006e3cf8265f573
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-28 16:03:24 +0800

    1 file modified

--- src/Body.svelte
+++ src/Body.svelte
@@ -4,10 +4,25 @@
   import CP from './Body-mod/client_portal.svelte'
   import Login from './Body-mod/login.svelte'
 
- const load = async ({ locals: { getSession } }) => {
-  return {
-    session: await getSession(),
-  }
+const  PUBLIC_SUPABASE_URL = 'https://amvtkeyfaduowfkbviyl.supabase.co'
+const  PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtdnRrZXlmYWR1b3dma2J2aXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2MDEzMTUsImV4cCI6MjAwMzE3NzMxNX0.n6XWDgimQY161SzrgcC9nSOzTAhMYHnstACz8nGtOVE'
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
+
+export const load = async ({ fetch, data, depends }) => {
+  depends('supabase:auth')
+
+  const supabase = createSupabaseLoadClient({
+    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    event: { fetch },
+    serverSession: data.session,
+  })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  return { supabase, session }
 }
 
 
