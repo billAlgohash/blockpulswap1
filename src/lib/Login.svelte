6a08ec400c6a4e004ae865fe9ccb4212e26ee75c
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-29 12:53:35 +0800

    2 files modified

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -14,6 +14,7 @@
         username,
         password,
         passwordConfirm: password,
+        name: 'Hi mom!',
       };
       const createdUser = await pb.collection('trader').create(data);
       await login();
