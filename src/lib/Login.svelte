4a5ae2e9586efaca687095ac67c55d7770845b53
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-30 15:10:15 +0800

    2 files modified

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -29,6 +29,8 @@
 
 </script>
 
+
+<div class="center">
 {#if $currentUser}
     <h3>Signed in as {$currentUser.username}.</h3>
     <p>USDT : {$currentUser.USDT}</p>
@@ -36,19 +38,39 @@
     <p>ETH : {$currentUser.ETH}</p>
     <button on:click={signOut}>Sign Out</button>
 {:else}
-  <form on:submit|preventDefault>
-    <input class="container"
-      placeholder="Username"
-      type="text"
-      bind:value={username}
-    />
+  <div class="big-container"><div class="container">
+      <form on:submit|preventDefault>
+        <input class="container"
+          placeholder="Username"
+          type="text"
+          bind:value={username}
+        />
 
-    <input class="container"
-      placeholder="Password" 
-      type="password" 
-      bind:value={password} 
-    />
-    <button on:click={signUp}>Sign Up</button>
-    <button on:click={login}>Login</button>
-  </form>
+        <input class="container"
+          placeholder="Password" 
+          type="password" 
+          bind:value={password} 
+        />
+        <button on:click={signUp}>Sign Up</button>
+        <button on:click={login}>Login</button>
+      </form>
+  </div></div>
 {/if}
+</div>
+
+<style>  
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+}
+
+/* 設定元素間距 */
+.container {
+  margin-bottom: 1em;
+  margin-top: 0.5em;
+}
+  </style>
