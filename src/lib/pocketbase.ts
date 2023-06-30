e95bcf314682205a79cfc6d2ea626a631df5a4b4
billAlgohash <124248581+billAlgohash@users.noreply.github.com>
2023-06-30 14:06:30 +0800

    1 file modified

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 import { writable }  from 'svelte/store';
 // export const pb = new PocketBase('https://ec2-35-88-34-102.us-west-2.compute.amazonaws.com:8080/');
 
-export const pb = new PocketBase('https://ec2-35-88-34-102.us-west-2.compute.amazonaws.com:3001/')
+export const pb = new PocketBase('https://blockpulse-swap.pockethost.io/')
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
